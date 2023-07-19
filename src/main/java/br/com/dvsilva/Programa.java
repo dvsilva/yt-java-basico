c2164940cd874c77dc3bcfdb3344a697b07f5ba3
dvsilva <danyllo.dvs@gmail.com>
2023-07-18 21:06:06 -0300

    aula 2 - criando primeiro programa

---
+++ src/main/java/br/com/dvsilva/Programa.java
@@ -0,0 +1,10 @@
+package br.com.dvsilva;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("outra frase!");
+    }
+
+}
